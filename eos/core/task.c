b53b202803ea151f4073f0e168a1d741ddc545cf
ironjr <jarin.lee@gmail.com>
2018-04-30 12:24:39 +0000

    First commit.

---
+++ eos/core/task.c
@@ -0,0 +1,132 @@
+/********************************************************
+ * Filename: core/task.c
+ * 
+ * Author: parkjy, RTOSLab. SNU.
+ * 
+ * Description: task management.
+ ********************************************************/
+#include <core/eos.h>
+
+#define READY		1
+#define RUNNING		2
+#define WAITING		3
+
+/*
+ * Queue (list) of tasks that are ready to run.
+ */
+static _os_node_t *_os_ready_queue[LOWEST_PRIORITY + 1];
+
+/*
+ * Pointer to TCB of running task
+ */
+static eos_tcb_t *_os_current_task;
+
+/*
+ * eos_create_task - Create task on the given entry point with arguments.
+ *  Allocated stack memory and task priority is given.
+ */
+int32u_t eos_create_task(eos_tcb_t *task, addr_t sblock_start, size_t sblock_size, void (*entry)(void *arg), void *arg, int32u_t priority) {
+    addr_t task_sp;
+    task_sp = _os_create_context(sblock_start, sblock_size, entry, arg);
+
+    task->state = READY;
+    task->priority = priority;
+    task->sp = task_sp;
+
+    /* Enqueue the new task to the ready queue */
+    task->node = (_os_node_t *)malloc(sizeof(_os_node_t));
+    task->node->ptr_data = task;
+    task->node->priority = priority;
+    _os_add_node_tail(&_os_ready_queue[priority], task->node);
+    _os_set_ready(priority);
+
+	PRINT("task: 0x%x, priority: %d\n", (int32u_t)task, priority);
+
+    return 0;
+}
+
+int32u_t eos_destroy_task(eos_tcb_t *task) {
+}
+
+/*
+ * eos_schedule - Scheduler dispatch new task from the ready queue. First it
+ *  saves the current context if exists, then it restores another context from
+ *  the next appropriate task.
+ */
+void eos_schedule() {
+    eos_tcb_t *current_task = eos_get_current_task();
+
+    if (current_task != NULL) {  
+        addr_t prev_task_sp;
+        prev_task_sp = _os_save_context();
+
+        if (prev_task_sp != NULL) {
+            int32u_t priority = current_task->priority;
+
+            /* Save current context to tcb */
+            current_task->sp = prev_task_sp;
+
+            /* Enqueue current task to the ready queue */
+            current_task->state = READY;
+            _os_add_node_tail(&_os_ready_queue[priority], current_task->node);
+            _os_set_ready(priority);
+        }
+    }
+
+    int32u_t work_priority = _os_get_highest_priority();
+    eos_tcb_t *next_task = _os_ready_queue[work_priority]->ptr_data;
+    if (_os_remove_node(_os_ready_queue[work_priority], next_task->node) == NULL) return;
+    
+    /* Restore the next context. */
+    _os_restore_context(next_task->sp);
+}
+
+eos_tcb_t *eos_get_current_task() {
+	return _os_current_task;
+}
+
+void eos_change_priority(eos_tcb_t *task, int32u_t priority) {
+}
+
+int32u_t eos_get_priority(eos_tcb_t *task) {
+}
+
+void eos_set_period(eos_tcb_t *task, int32u_t period){
+}
+
+int32u_t eos_get_period(eos_tcb_t *task) {
+}
+
+int32u_t eos_suspend_task(eos_tcb_t *task) {
+}
+
+int32u_t eos_resume_task(eos_tcb_t *task) {
+}
+
+void eos_sleep(int32u_t tick) {
+}
+
+void _os_init_task() {
+	PRINT("initializing task module.\n");
+
+	/* init current_task */
+	_os_current_task = NULL;
+
+	/* init multi-level ready_queue */
+	int32u_t i;
+	for (i = 0; i < LOWEST_PRIORITY; i++) {
+		_os_ready_queue[i] = NULL;
+	}
+}
+
+void _os_wait(_os_node_t **wait_queue) {
+}
+
+void _os_wakeup_single(_os_node_t **wait_queue, int32u_t queue_type) {
+}
+
+void _os_wakeup_all(_os_node_t **wait_queue, int32u_t queue_type) {
+}
+
+void _os_wakeup_sleeping_task(void *arg) {
+}
---
+++ eos/hal/linux/context.c
@@ -0,0 +1,109 @@
+#include <core/eos.h>
+#include <core/eos_internal.h>
+
+/*
+ * How EOS saves a context of a task in the stack memory.
+ */
+typedef struct _os_context {
+	/* low address */
+    int32u_t edi_reg;
+    int32u_t esi_reg;
+    int32u_t ebp_reg;
+    int32u_t esp_reg;
+    int32u_t ebx_reg;
+    int32u_t edx_reg;
+    int32u_t ecx_reg;
+    int32u_t eax_reg;
+    int32u_t eflags_reg;
+    int32u_t eip_reg;
+	/* high address */	
+} _os_context_t;
+
+/*
+ * print_context - For debugging. Print all the contents of the given context
+ *  in the order they resident in the memory.
+ */
+void print_context(addr_t context) {
+	if (context == NULL) return;
+	_os_context_t *ctx = (_os_context_t *)context;
+
+	PRINT("eax    = 0x%x\n", ctx->eax_reg);
+	PRINT("ebx    = 0x%x\n", ctx->ebx_reg);
+	PRINT("ecx    = 0x%x\n", ctx->ecx_reg);
+	PRINT("edx    = 0x%x\n", ctx->edx_reg);
+	PRINT("edi    = 0x%x\n", ctx->edi_reg);
+	PRINT("esi    = 0x%x\n", ctx->esi_reg);
+	PRINT("ebp    = 0x%x\n", ctx->ebp_reg);
+	PRINT("esp    = 0x%x\n", ctx->esp_reg);
+	PRINT("eflags = 0x%x\n", ctx->eflags_reg);
+	PRINT("eip    = 0x%x\n", ctx->eip_reg);
+}
+
+/*
+ * _os_create_context - Create new empty context based on the entry of main
+ *  routine of the new task. Returns new stack pointer.
+ */
+addr_t _os_create_context(addr_t stack_base, size_t stack_size, void (*entry)(void *), void *arg) {
+    addr_t stack_bottom = (addr_t)(((int32u_t)stack_base) + stack_size);
+    void **arg_param = ((void **)stack_bottom) - 1;
+    addr_t *ret_addr = ((addr_t *)arg_param) - 1;
+    _os_context_t *ctx = ((_os_context_t *)ret_addr) - 1;
+
+    PRINT("stack_base   = 0x%x\n", stack_base);
+    PRINT("stack_size   = %u\n", stack_size);
+    PRINT("stack_bottom = 0x%x\n", stack_bottom);
+    PRINT("arg_param    = 0x%x\n", arg_param);
+    PRINT("ret_address  = 0x%x\n", ret_addr);
+
+    /* Initialize the new stack */
+    *arg_param = arg;
+    *ret_addr = NULL;
+    ctx->eax_reg = (int32u_t)NULL;
+    ctx->ebx_reg = (int32u_t)NULL;
+    ctx->ecx_reg = (int32u_t)NULL;
+    ctx->edx_reg = (int32u_t)NULL;
+    ctx->edi_reg = (int32u_t)NULL;
+    ctx->esi_reg = (int32u_t)NULL;
+    ctx->ebp_reg = (int32u_t)NULL;
+    ctx->esp_reg = (int32u_t)NULL;
+    ctx->eflags_reg = (int32u_t)NULL;
+    ctx->eip_reg = (int32u_t)entry;
+
+    print_context(ctx);
+
+    return (addr_t)(ctx - 1) + 1;
+}
+
+/*
+ * _os_restore_context - Restore context based on the stack pointer. First it
+ *  begins with the given stack, restores all the registers those have been
+ *  saved. The routine finalizes with returning to the last saved state of
+ *  the indicated execution stream.
+ */
+void _os_restore_context(addr_t sp) {
+    __asm__ __volatile__("\
+        mov %0, %%esp;\
+        popa;\
+        pop _eflags;\
+        ret;"
+        ::"m"(sp));
+}
+
+/*
+ * _os_save_context - Saves all the (integral) registers on the stack. Returns
+ *  the stack pointer, and the current thread of control is stopped. When the
+ *  context is restored, the execution begins right after the first return
+ *  statement, where the second return is held. The routine then returns 0.
+ */
+addr_t _os_save_context() {
+    __asm__ __volatile__("\
+        push $resume_eip;\
+        push _eflags;\
+        pusha;\
+        mov %%esp, %%eax;\
+        push 0x4(%%esp);\
+        push 0x0(%%ebp);\
+        mov %%esp, %%ebp;\
+    resume_eip:":);
+    return NULL;
+}
